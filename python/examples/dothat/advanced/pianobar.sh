2a45cca453355adf35b36c7f06841d4262f48c07
Kenneth Seal <hunzinker@gmail.com>
2015-12-01 10:39:45 -0700

    Send commands to pianobar via named pipe

--- python/examples/dothat/advanced/pianobar.sh
+++ python/examples/dothat/advanced/pianobar.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-su - pi -c "/home/pi/bin/headless_pianobar" > /dev/null &
-sleep 0.5
-su - pi -c "/bin/pidof pianobar"
+if [ -z $1 ]; then
+    su - pi -c "/home/pi/bin/headless_pianobar" > /dev/null &
+    sleep 0.5
+    su - pi -c "/bin/pidof pianobar"
+else
+    su - pi -c "echo -ne \"${1}\" > /home/pi/.config/pianobar/ctl"
+fi
